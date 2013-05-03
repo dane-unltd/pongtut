7463d3f140638fb078ebfa305cd114bf9e04c71d
dane <david_neumann@gmx.net>
2013-05-03 14:52:10 +0200

    update

--- vec.go
+++ vec.go
@@ -24,3 +24,29 @@ func (a *Vec) Equals(b *Vec) bool {
 	}
 	return true
 }
+
+func (res *Vec) Clamp(s *Vec) {
+	for i := range *res {
+		if (*res)[i] > (*s)[i]/2 {
+			(*res)[i] = (*s)[i] / 2
+		}
+		if (*res)[i] < -(*s)[i]/2 {
+			(*res)[i] = -(*s)[i] / 2
+		}
+	}
+}
+
+func Dot(a, b *Vec) float64 {
+	return (*a)[0]*(*b)[0] + (*a)[1]*(*b)[1] + (*a)[2]*(*b)[2]
+}
+
+func (v *Vec) Nrm2Sq() float64 {
+	return Dot(v, v)
+}
+
+func (res *Vec) Scale(alpha float64, v *Vec) *Vec {
+	(*res)[0] = alpha * (*v)[0]
+	(*res)[1] = alpha * (*v)[1]
+	(*res)[2] = alpha * (*v)[2]
+	return res
+}
